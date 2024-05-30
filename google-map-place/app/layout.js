be4a24c2a1b78686066d92a7f4869309d9594d46
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-05-29 19:16:20 -0700

    Finished OAuth using Google

--- google-map-place/app/layout.js
+++ google-map-place/app/layout.js
@@ -13,9 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider>
-          {children}
-        </Provider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
--- google-map-place/app/page.js
+++ google-map-place/app/page.js
@@ -1,9 +1,22 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session?.user) {
+      router.push("/Login");
+    }
+  }, [session]);
   return (
     <div>
       <h2>Bruh</h2>
+      <button onClick={() => signOut()}>SignOut</button>
     </div>
   );
 }
