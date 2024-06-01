8b1264876851fd30e41d826f5886e9e439f67877
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-06-01 13:31:32 -0700

    Finished CategoryList

--- google-map-place/app/page.js
+++ google-map-place/app/page.js
@@ -3,6 +3,7 @@ import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import CategoryList from "./components/Home/CategoryList";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -15,10 +16,11 @@ export default function Home() {
   }, [session]);
   return (
     <div>
-      <h2>Bruh</h2>
       <button onClick={() => signOut()}>SignOut</button>
       <div className="grid grid-cols-4 h-screen">
-        <div className="bg-red-300">First</div>
+        <div className="p-3">
+          <CategoryList />{" "}
+        </div>
         <div className="bg-blue-300 col-span-3">Second</div>
       </div>
     </div>
